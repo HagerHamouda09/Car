0890a95670b83dba9f4bdbd92c2f2b95d64bb829
HagerAdel09 <es-Hager.Hamouda2026@alexu.edu.eg>
2026-06-20 18:09:28 +0300

    Updatedscenerios

--- Vehicle/Main/Main.ino
+++ Vehicle/Main/Main.ino
@@ -1,227 +1,3 @@
-// #include "config.h"
-// #include "Motor.h"
-// #include "Relay.h"
-// #include "Button.h"
-// #include "Buzzer.h"
-// #include "Servo.h"
-// #include "Hall.h"
-// #include "MPU.h"
-// #include "Ultrasonic.h"
-// #include "BLE.h"
-
-// // SYSTEM_CASES systemState = SYSTEM_IDLE;
-// SYSTEM_CASES systemState = SYSTEM_NORMAL;
-
-// bool motorLocked = false;
-
-// int Trials = 0;
-
-// void setup() {
-//   Serial.begin(115200);
-//   delay(1000);
-//   Serial.println("ESP32 is running!");
-//   Servo_Init();
-//   Motor_Init();
-//   Relay_Init();
-//   Button_Init();
-//   Buzzer_Init();
-//   Hall_Init();
-//   MPU_init();
-//   Ultrasonic_Init();
-//   bleCar.begin();
-// }
-
-// void ControlSpeed(int x, int y) {
-//   if (x == 1) {
-//     Serial.print("reducing speed suddenly");
-//        motorLocked = true; 
-//     Set_Motor_Speed(150);
-//     Set_Motor_Speed(75);
-//     Set_Motor_Speed(0);
-
-//     if (Crash_Detect()) {
-//       systemState = SYSTEM_CRASH;
-//       return;
-//     }
-//   }
-
-//   else if (x == 2) {
-//     motorLocked = true; 
-//     for (int i = 1; i <= 5; i++) {
-//       Serial.print("reducing speed ");
-//       Set_Motor_Speed(250 - (50 * i));
-//       if (Crash_Detect()) {
-//         systemState = SYSTEM_CRASH;
-//         return;
-//       }
-//       delay(1000);
-//     }
-//     delay(2000);
-//   }
-
-// // safe stop variable 
-// // driver stopped the car himself (implement(traffic or something))
-//   // 0 is safe
-//   if (y == 1)
-//   {
-//     Serial.println("OBBBBBBBSTT");
-//     systemState = SYSTEM_OBSTACLESTOP;
-
-//   }
-//   else
-//   {
-//     systemState = SYSTEM_SAFESTOP;
-
-//   }
-// }
-
-
-
-// void loop() {
-//   bleCar.handleReconnect();
-
-//   // systemState = SYSTEM_NORMAL;
-//          Calc_Speed();
-
-//       int obs = Obstacle();
-//       int speed = Get_Speed();
-//       int crash = Crash_Detect();
-//       int distance = Get_Distance();
-  
-//   // bleCar.sendTelemetry(systemState, speed, distance);
-//     bleCar.sendTelemetry(systemState);
-
-
-//   switch (systemState) {
-//     case SYSTEM_IDLE:
-//     //this delay represents the wait state in the state machine if the vitals were not ok
-//       //delay(5000); (will be handled in the mobile )
-//       //Need system check so when driver wears the band start reading vitals and send it to mobile 
-      
-//       break;
-
-//     case SYSTEM_READY:
-//       Relay_ON();
-//       break;
-
-//     case SYSTEM_NORMAL:{
-//       Relay_ON();
-//       // if (!motorLocked)
-//       // {
-//       // Set_Motor_Speed(250);
-//       // }      
-
-//                  motorLocked = false;
-//                 Set_Motor_Speed(250);
-
-//       // Serial.println(speed);
-//       // Serial.println(obs);
-//       // Serial.println(Get_Distance());
-//       // Serial.println("DO WE HAVE A CRASH??");
-//       // Serial.println(crash);
-//       // Serial.println("DO WE HAVE A CRASH??");
-
-//       while (!crash && speed != 0 && obs== 3 && systemState == SYSTEM_NORMAL)  {
-          
-//                  motorLocked = false;
-//                 Set_Motor_Speed(250);
-
-//       obs = Obstacle();
-//       speed = Get_Speed();
-//       crash = Crash_Detect();
-//       distance = Get_Distance();
-      
-//         Serial.print("normal");
-//         Calc_Speed();
-
-//         // bleCar.sendTelemetry(systemState, speed, distance);
-//         bleCar.sendTelemetry(systemState);
-
-//         delay(60);
-//       }
-//       if (crash) {
-//         systemState = SYSTEM_CRASH;
-//         Serial.print("crash detected");
-//         break;
-//       }
-//       if (speed == 0) {
-//         systemState = SYSTEM_SAFESTOP;
-//         Serial.print("safe stop");
-
-//         break;
-//       }
-//      // 1 obstacle
-//       if (obs != 3)
-//         ControlSpeed(obs, 1);
-
-//       break;
-//   }
-
-//     case SYSTEM_OBSTACLESTOP:
-//       obs = Obstacle();
-
-//         if (obs == 3)
-//         {
-//           motorLocked = false;
-//           systemState = SYSTEM_NORMAL;
-//         }
-
-//     break;
-
-//     case SYSTEM_SAFESTOP:
-//       Calc_Speed();
-//       speed = Get_Speed();
-
-//       Serial.print("Case Safestop:");
-//       Serial.print(speed);
-
-//         if (speed > 0)
-//         {
-//           motorLocked = false;
-//         systemState = SYSTEM_NORMAL;
-//         }
-      
-//     break;
-
-//     case SYSTEM_CRASH:
-//       Set_Motor_Speed(0);
-//       Relay_Off();
-//       Buzzer_On();
-//       //INFORM MOBILE THERE IS A CRASH
-//       //flag to stop loop
-//       //sleep mode for esp
-//             motorLocked = true;
-
-//       break;
-
-//     case SYSTEM_EMERGENCY:
-          
-
-//             //Set_Servo_Angle(0);
-
-//             Set_Servo_Angle(110);
-      
-//       if (Get_Button_State()) 
-//       {
-//         motorLocked = true;
-//         Buzzer_On();
-
-//         //mafesh obstacle+aw2f (emergecncy)
-//         if (obs == 3)
-//           ControlSpeed(2, 1);
-//         else
-//           //obstacle +aw2f basor3a
-//           //obstacle +aw2f blraha
-//           ControlSpeed(obs, 1);
-      
-//       Relay_Off();
-//             Set_Servo_Angle(90);
-//       }
-//       break;
-//   }
-// }
-
-
 // // 24/5 code (system check+ ) 
 
 #include "config.h"
@@ -235,7 +11,6 @@
 #include "Ultrasonic.h"
 #include "BLE.h"
 #include "SelfTest.h"
-#include "LED.h"
 
 // SYSTEM_CASES systemState = SYSTEM_IDLE;
 SYSTEM_CASES systemState = SYSTEM_CHECK;
@@ -253,7 +28,6 @@ void setup() {
   Relay_Init();
   Button_Init();
   Buzzer_Init();
-  LED_Init();
   Hall_Init();
   MPU_init();
   Ultrasonic_Init();
@@ -459,7 +233,7 @@ void loop() {
       Set_Motor_Speed(0);
       Relay_Off();
       Buzzer_On();
-      LED_On();
+
       //INFORM MOBILE THERE IS A CRASH
       //flag to stop loop
       //sleep mode for esp
@@ -478,7 +252,6 @@ void loop() {
       {
         motorLocked = true;
         Buzzer_On();
-        LED_On();
 
         //mafesh obstacle+aw2f (emergecncy)
         if (obs == 3)
--- Vehicle/Main/Servo.cpp
+++ Vehicle/Main/Servo.cpp
@@ -1,23 +1,3 @@
-// #include <Arduino.h>
-
-// #include <ESP32Servo.h>
-
-// #include "Servo.h"
-
-// Servo myServo;
-
-// void Servo_Init()
-// {
-//   myServo.attach(SERVO, 500, 2400);
-//   myServo.write(90);
-// }
-
-// void Set_Servo_Angle(int angle)
-// {
-//   myServo.write(angle); 
-
-// }
-
 #include <Arduino.h>
 #include <ESP32Servo.h>
 #include "Servo.h"
